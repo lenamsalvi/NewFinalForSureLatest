98e1d4d56cf716221ca9df10a9614247ff446a4f
gabe-gardyn <gabriel@gardyn.tech>
2024-11-02 23:56:28 -0400

    refactor window as Class-based

--- window.py
+++ window.py
@@ -5,37 +5,49 @@ HEIGHT = 400
 DEFAULT_FONT = ("Helvetica", 16)
 
 
-def main():
-    # Main setup
-    root = tk.Tk()
-    root.title("Autonomous Landscape Design")
-    root.geometry(f"{WIDTH}x{HEIGHT}")
+class LandscapeDesignApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-    # Content structure
-    content_frame = tk.Frame(root, highlightbackground="white", highlightthickness=2)
-    content_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-    content_frame_label = tk.Label(
-        content_frame, text="Content Frame Label", font=DEFAULT_FONT
-    )
-    content_frame_label.pack(pady=20)
+        # Main window setup
+        self.title("Autonomous Landscape Design")
+        self.geometry(f"{WIDTH}x{HEIGHT}")
 
-    # Controls struture
-    control_frame = tk.Frame(root, highlightbackground="white", highlightthickness=2)
-    control_frame.pack(side=tk.BOTTOM, fill=tk.X)
+        # Build UI components
+        self.create_content_frame()
+        self.create_control_frame()
 
-    control_frame_label = tk.Label(
-        control_frame, text="Control Frame Label", font=DEFAULT_FONT
-    )
-    control_frame_label.pack(pady=20)
+    def create_content_frame(self):
+        self.content_frame = tk.Frame(
+            self, highlightbackground="white", highlightthickness=2
+        )
+        self.content_frame.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
-    control_frame_button = tk.Button(
-        control_frame, text="Button", command=lambda: print("Button clicked")
-    )
-    control_frame_button.pack(side=tk.BOTTOM, padx=10, pady=10)
+        content_frame_label = tk.Label(
+            self.content_frame, text="Content Frame Label", font=DEFAULT_FONT
+        )
+        content_frame_label.pack(pady=20)
 
-    # Main loop
-    root.mainloop()
+    def create_control_frame(self):
+        self.control_frame = tk.Frame(
+            self, highlightbackground="white", highlightthickness=2
+        )
+        self.control_frame.pack(side=tk.BOTTOM, fill=tk.X)
+
+        control_frame_label = tk.Label(
+            self.control_frame, text="Control Frame Label", font=DEFAULT_FONT
+        )
+        control_frame_label.pack(pady=20)
+
+        control_frame_button = tk.Button(
+            self.control_frame, text="Button", command=self.on_button_click
+        )
+        control_frame_button.pack(side=tk.BOTTOM, padx=10, pady=10)
+
+    def on_button_click(self):
+        print("Button clicked")
 
 
 if __name__ == "__main__":
-    main()
+    app = LandscapeDesignApp()
+    app.mainloop()
